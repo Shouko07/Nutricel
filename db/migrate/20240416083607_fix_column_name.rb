b13d314b34a901f31c3fa514e49e90d72fef312f
Marcos Montaño <marcosmontano0707@gmail.com>
2024-04-16 22:05:34 -0700

    segundo intento creacion tabla pedidos

--- db/migrate/20240416083607_fix_column_name.rb
+++
@@ -1,7 +0,0 @@
-class FixColumnName < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :pedidos, :pedido_id, :usuario_id
-    # Renombrar el índice también
-    rename_index :pedidos, 'index_pedidos_on_pedido_id', 'index_pedidos_on_usuario_id' 
-  end
-end
