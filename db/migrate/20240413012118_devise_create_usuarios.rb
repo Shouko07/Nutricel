bfcfb8cbcad3a36c70a7521a30c29d4c6c3140cb
Marcos Montaño <marcosmontano0707@gmail.com>
2024-04-16 18:44:24 -0700

    Revert "intento de migracion"
    This reverts commit 672da8d1059cf53a6db3578fc25189cf0c549499.

---
+++ db/migrate/20240413012118_devise_create_usuarios.rb
@@ -0,0 +1,44 @@
+# frozen_text_literal: true
+
+class DeviseCreateUsuarios < ActiveRecord::Migration[7.1]
+  def change
+    create_table :usuarios do |t|
+      ## Database authenticatable
+      t.text :email,              null: false, default: ""
+      t.text :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.text   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.text   :current_sign_in_ip
+      # t.text   :last_sign_in_ip
+
+      ## Confirmable
+      # t.text   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.text   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.text   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+
+      t.timestamps null: false
+    end
+
+    add_index :usuarios, :email,                unique: true
+    add_index :usuarios, :reset_password_token, unique: true
+    # add_index :usuarios, :confirmation_token,   unique: true
+    # add_index :usuarios, :unlock_token,         unique: true
+  end
+end
