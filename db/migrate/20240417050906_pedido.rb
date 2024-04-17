6f4223f60824f1ef50ea0f15c657f741f89f4cbd
Marcos Montaño <marcosmontano0707@gmail.com>
2024-04-16 22:24:59 -0700

    borrado tabla pedidos

--- db/migrate/20240417050906_pedido.rb
+++ db/migrate/20240417050906_pedido.rb
@@ -1,11 +1,5 @@
 class Pedido < ActiveRecord::Migration[7.1]
   def change
-    create_table :pedidos do |t|
-      t.integer :cantidad_ordenada
-      t.datetime :fecha_inicial
-      t.datetime :fecha_entrega
-      t.integer :usuario_id
-      t.timestamps
-    end
+    drop_table :pedidos
   end
 end
